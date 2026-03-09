16883ba26e5cb6a91d6bdf66011ff9a1c9997077
Hientrietman <vanqui8028@gmail.com>
2026-03-09 21:22:18 +0700

    implement simple direct and fan-out queue

---
+++ src/main/java/com/prelude/rabbitmq/controller/ProducerController.java
@@ -0,0 +1,38 @@
+package com.prelude.rabbitmq.controller;
+
+import com.prelude.rabbitmq.configuration.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api")
+public class ProducerController {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/")
+    public String sendMessage(@RequestBody Map<String, Object> message){
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                RabbitMQConfig.ROUTING_KEY,
+                message
+        );
+        return "Message Sent!";
+    }
+
+    @PostMapping("/fan-out")
+    public String sendMessageFanOut(@RequestBody Map<String, Object> message){
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.FANOUT_EXCHANGE,
+                "",
+                message
+        );
+        return "Message Sent!";
+    }
+}
---
+++ src/main/java/com/prelude/rabbitmq/service/ConsumerService.java
@@ -0,0 +1,28 @@
+package com.prelude.rabbitmq.service;
+
+import com.prelude.rabbitmq.configuration.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ConsumerService {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void receiveMessage(Map<String, Object> message) {
+        System.out.println("Đã nhận tin nhắn từ Queue: " + message.get("message"));
+        // Bạn có thể xử lý logic CRUD, gửi mail... ở đây
+    }
+    // Lắng nghe Queue A của Fanout
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_A)
+    public void consumeA(Map<String, Object> message) {
+        System.out.println("Consumer A nhận được: " + message);
+    }
+
+    // Lắng nghe Queue B của Fanout
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_B)
+    public void consumeB(Map<String, Object> message) {
+        System.out.println("Consumer B nhận được: " + message);
+    }
+}
